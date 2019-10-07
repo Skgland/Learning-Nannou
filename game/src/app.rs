1e4d65109dfdaa963dc89bfa699ea1c751889b6b
BB20101997 <bb-github@t-online.de>
2019-10-07 19:49:01 +0200

    some updates and reworking of stuff

--- game/src/app.rs
+++ game/src/app.rs
@@ -7,16 +7,17 @@ use crate::{gui::*,  game::GameState, TextureMap};
 use conrod_core::{
     Borderable,
     color::Colorable,
-    input::RenderArgs,
     widget,
     widget::Widget,
 };
 
 use opengl_graphics::GlGraphics;
-use piston::input::*;
-pub use piston::window::*;
 use piston_window::{texture::UpdateTexture, PistonWindow};
+use piston::input::{Input, UpdateArgs, RenderArgs, Event};
 use glutin_window::GlutinWindow;
+use conrod_core::input::Key;
+use learning_conrod_base::{RenderContext, Application};
+use piston::event_loop::{Events};
 
 pub struct App {
     pub gui: GUI,
@@ -47,15 +48,13 @@ impl Action {
 
 type G = opengl_graphics::GlGraphics;
 
-impl App {
 
-    pub fn new(gui: GUI, texture_map: TextureMap<G>, init_menu:MenuState) -> Self {
-        App { gui, texture_map, current_menu:init_menu}
-    }
+impl Application for App {
+    type RR = ();
+    type IR = ();
+    type UR = UpdateAction;
 
-    pub fn render(&self, context: &mut RenderContext<G>, args: &RenderArgs) {
-
-
+    fn render(&self, context: &mut RenderContext<G>, args: &RenderArgs) {
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
         const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
 
@@ -83,8 +82,7 @@ impl App {
 
 
         gl.draw(args.viewport(), |c, gl| {
-
-            self.current_menu.draw_raw(args,c,gl,&self.texture_map);
+            self.current_menu.draw_raw(args, c, gl, &self.texture_map);
 
             let view = c.store_view();
 
@@ -100,7 +98,7 @@ impl App {
         });
     }
 
-    pub fn input(&mut self, event: Input, window: &mut PistonWindow<GlutinWindow>) {
+    fn input(&mut self, event: Input, event_loop: &mut Events, window: &mut PistonWindow<GlutinWindow>) {
         if let Some(cr_event) = conrod_piston::event::convert(Event::Input(event.clone()), self.gui.ui.win_w, self.gui.ui.win_h) {
             self.gui.ui.handle_event(cr_event);
         }
@@ -109,39 +107,25 @@ impl App {
 
     }
 
-
-    pub fn toggle_fullscreen(window: &mut PistonWindow<GlutinWindow>, current: &mut bool) {
-        if *current {
-            window.window.window.set_fullscreen(None);
-            *current = false;
-        } else {
-            let monitor = window.window.window.get_primary_monitor();
-            window.window.window.set_fullscreen(Some(monitor));
-            *current = true;
-        }
-    }
-
-    pub fn update(&mut self, args: UpdateArgs, window: &mut PistonWindow<GlutinWindow>) {
-
-
+    fn update(&mut self, args: &UpdateArgs, window: &mut PistonWindow<GlutinWindow>) -> UpdateAction {
         let ui = &mut self.gui.ui.set_widgets();
 
-        {
-            use conrod_core::event::{Event, Ui, Release, Button};
-            for event in ui.global_input().events() {
-                if let Event::Ui(event) = event {
-                    match event {
-                        Ui::Release(_, Release { button: Button::Keyboard(Key::F11), .. }) => { Self::toggle_fullscreen(window, &mut self.gui.fullscreen) }
-                        Ui::Release(_, Release { button: Button::Keyboard(Key::Escape), .. }) => {
-                            self.current_menu.handle_esc(window);
+        use conrod_core::event::{Event, Ui, Release, Button};
+        for event in ui.global_input().events() {
+            if let Event::Ui(event) = event {
+                match event {
+                    Ui::Release(_, Release { button: Button::Keyboard(Key::F11), .. }) => { Self::toggle_fullscreen(window, &mut self.gui.fullscreen) }
+                    Ui::Release(_, Release { button: Button::Keyboard(Key::Escape), .. }) => {
+                        if let UpdateAction::Close = self.current_menu.handle_esc(window) {
+                            return UpdateAction::Close
                         }
-                        Ui::Release(_, Release { button: Button::Keyboard(Key::F1), .. }) => {
-                            /*if let HUD(state) | OverlayMenu(_, state) = &mut self.gui.active_menu {
-                                self.gui.active_menu = GameOnly(state.clone());
-                            }*/
-                        }
-                        _ => ()
                     }
+                    Ui::Release(_, Release { button: Button::Keyboard(Key::F1), .. }) => {
+                        /*if let HUD(state) | OverlayMenu(_, state) = &mut self.gui.active_menu {
+                            self.gui.active_menu = GameOnly(state.clone());
+                        }*/
+                    }
+                    _ => ()
                 }
             }
         }
@@ -149,6 +133,35 @@ impl App {
         //necessary so that when we stop drawing anything in F1 mode, Resize events will still be processed
         widget::canvas::Canvas::new().border_rgba(0.0, 0.0, 0.0, 0.0).rgba(0.0, 0.0, 0.0, 0.0).set(self.gui.ids.main_canvas, ui);
 
-         self.current_menu.update(ui,&mut self.gui.ids,args)
+        self.current_menu.update(ui, &mut self.gui.ids, args);
+
+        UpdateAction::Nothing
     }
+}
+
+impl App {
+
+    pub fn new(gui: GUI, texture_map: TextureMap<G>, init_menu:MenuState) -> Self {
+        App { gui, texture_map, current_menu:init_menu}
+    }
+
+    pub fn toggle_fullscreen(window: &mut PistonWindow<GlutinWindow>, current: &mut bool) {
+        //TODO how to do this again
+        if *current {
+            //window.window.window.set_fullscreen(None)
+            *current = false;
+        } else {
+            //window.window.window.set_fullscreen(Some(monitor));
+            *current = true;
+        }
+    }
+
+
+
+
+}
+
+pub enum UpdateAction {
+    Nothing,
+    Close
 }
